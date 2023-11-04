a34ca55babb42b5e420102524649f7714cfef057
Dat Nguyen <darrennguyen238@gmail.com>
2023-11-04 15:44:11 -0700

    First FrontEnd Prototype

---
+++ checkGithubLink.js
@@ -0,0 +1,44 @@
+// Asynchronous function to check for a GitHub URL using regex
+// Input: url (string)
+// Output: isValid (boolean)
+
+/**
+ * Checks if the provided string is a valid GitHub URL
+ * @async
+ * @function isGitHubUrl
+ * @param {string} url - The string to check
+ * @returns {Promise<boolean>} - A Promise that resolves with a boolean indicating whether the string is a valid GitHub URL
+ * @throws {Error} - If there was an error in the regex process
+ */
+async function isGitHubUrl(url) {
+    try {
+        // Log the URL being tested
+        console.log(`Testing URL: ${url}`);
+
+        // Regular expression for matching GitHub URLs
+        const githubUrlRegex = /^(https?:\/\/)?(www\.)?github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_.-]+(?:\/)?$/;
+        // console.log(githubUrlRegex.test('https://github.com/spy16/droplets')); // This should log 'true'
+
+        // Test the URL against the regex asynchronously
+        const isValid = await new Promise((resolve, reject) => {
+            try {
+                const result = githubUrlRegex.test(url);
+                console.log(`Regex test result for ${url}: ${result}`); // Log the test result
+                resolve(result);
+            } catch (error) {
+                console.error(`Regex test error for ${url}: ${error}`);
+                reject(error);
+            }
+        });
+
+        return isValid; // Return the result of the regex test
+    } catch (error) {
+        console.error(`Error checking GitHub URL ${url}: ${error}`);
+        throw error; // Throw the error to be handled by the caller
+    }
+}
+
+// Export the functions for use in other modules
+module.exports = {
+    isGitHubUrl,
+};
--- index.js
+++ index.js
@@ -4,3 +4,49 @@ const sqlite = require("sqlite"); // Import SQLite library for interfacing with 
 const express = require("express"); // Import Express.js web framework
 const { engine } = require("express-handlebars"); // Import the engine function from express-handlebars for rendering views
 const cookierParser = require("cookie-parser"); // Import cookie-parser for parsing cookies
+
+// Import functions from the "./helpers/db" module
+const {
+    isGitHubUrl,
+} = require("./checkGithubLink.js");
+
+const path = require("path"); // Import path module for working with file and directory paths
+
+const app = express(); // Initialize the Express application
+const port = 3000; // Set the server port to 3000
+
+// Configure the Express app
+app.engine("handlebars", engine()); // Set the view engine to handlebars
+app.set("view engine", "handlebars"); // Set handlebars as the view engine
+app.set("views", __dirname + "/views"); // Set the directory for views
+
+app.use("/static", express.static(__dirname + "/static")); // Serve static files from the "static" directory
+app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies in a URL-encoded format
+app.use(cookierParser()); // Use cookie-parser middleware to parse cookies
+
+// Handle GET request to the root route
+app.get("/", (req, res) => {
+    let error = req.query.error; // Get error query parameter, if any
+    res.render("home", { layout: false, error: error }); // Render the "home" view and pass the error value
+});
+
+
+
+// Handle POST request to checking URL
+app.post("/api/check-github-url", async (req, res) => {
+    let urlToCheck = req.body.url; // Assuming the URL is sent in the POST request body
+    let url = await isGitHubUrl(urlToCheck);
+
+    if (url) {
+        res.cookie("url", url); // Set a cookie with the username
+        res.redirect("/"); // Redirect to the tasks route upon successful login
+    } else {
+        res.redirect("/?error=Invalid url")
+    }
+});
+
+
+// Start the server on the specified port
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`); // Log the server's listening status
+});
